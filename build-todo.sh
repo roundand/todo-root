237bde1cb23d92090abf7b669764c0a441cee94b
roundand <francis.norton@gmail.com>
2015-04-14 16:27:52 +0100

    corrected mis-matched directory names

--- build-todo.sh
+++ build-todo.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 git clone git@github.com:roundand/todo-web.git ./todo-web
 git clone git@github.com:roundand/todo-server.git ./todo-server
-docker-compose kill && docker-compose build && docker-compose up -d && curl 127.0.0.1/todo
+docker-compose kill
+docker-compose build
+docker-compose up -d
+curl 127.0.0.1/todo
