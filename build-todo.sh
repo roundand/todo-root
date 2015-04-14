c72445c47e29dae09e0ff55f724f57d475c84055
roundand <francis.norton@gmail.com>
2015-04-14 17:43:48 +0100

    added 30 second sleep after build step before up step in order to get this running first time from install

--- build-todo.sh
+++ build-todo.sh
@@ -3,5 +3,6 @@ git clone git@github.com:roundand/todo-web.git ./todo-web
 git clone git@github.com:roundand/todo-server.git ./todo-server
 docker-compose kill
 docker-compose build
+sleep 30
 docker-compose up -d
 curl 127.0.0.1/todo
