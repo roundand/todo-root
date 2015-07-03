ae60cd94b6f9ca0d1d6d6854d2140ad4c05a42f3
fn <fn@fnorton-centos7.ie.com>
2015-07-03 14:31:57 +0100

    how to import proxy environmental settings into new containers...

--- build-todo.sh
+++ build-todo.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+
+# clone the two sub-projects to known locations
 git clone https://github.com/roundand/todo-web.git ./todo-web
 git clone https://github.com/roundand/todo-server.git ./todo-server
+
+# clear the decks for a (re)build
 docker-compose kill
+
+# copy proxy settings to a file for inclusion in containers
+set | grep -i _proxy= > proxies.env
+
+# rebuild, and wait for completion
 docker-compose build
 sleep 30
+
+# fire 'em up
 docker-compose up -d
+
+# check it's working!
 curl 127.0.0.1/todo
